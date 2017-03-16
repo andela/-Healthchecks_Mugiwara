104d846f99e63e6de4d0883b58458e521e021125
Jonathan Ngondi Munyi <jonathan.ngondi@andela.com>
2017-03-16 15:06:31 +0300

    Add config settings for db

--- hc/settings.py
+++ hc/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
+import dj_database_url
 import os
 import warnings
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
