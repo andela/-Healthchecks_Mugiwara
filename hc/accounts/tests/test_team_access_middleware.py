e403c353010972b3d2acb8446ad02198055ac27e
Jonathan Ngondi Munyi <jonathan.ngondi@andela.com>
2017-03-27 16:06:46 +0300

    Added assertion statement for Profile counts

--- hc/accounts/tests/test_team_access_middleware.py
+++ hc/accounts/tests/test_team_access_middleware.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import TestCase, tag
 from hc.accounts.models import Profile
 
 
 class TeamAccessMiddlewareTestCase(TestCase):
 
+    @tag('missing_profiles')
     def test_it_handles_missing_profile(self):
         user = User(username="ned", email="ned@example.org")
         user.set_password("password")
@@ -15,3 +16,5 @@ class TeamAccessMiddlewareTestCase(TestCase):
         self.assertEqual(r.status_code, 200)
 
         ### Assert the new Profile objects count
+        user = User.objects.count()
+        self.assertEqual(user, 1, "Should return 1 as the value of user profiles.")
